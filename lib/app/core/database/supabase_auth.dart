27f09074fa03f3bbab0a795adde0737ae675a9e0
brenoguimaraes <brenoguimaraes@mpto.mp.br>
2024-09-12 14:58:16 -0300

    fix

--- lib/app/core/database/supabase_auth.dart
+++ lib/app/core/database/supabase_auth.dart
@@ -2,8 +2,10 @@ import 'package:recicla_palmas/app/core/database/supabase_db.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseAuth {
-  static Future<bool> signUp(
-      {required String email, required String password}) async {
+  static Future<bool> signUp({
+    required String email,
+    required String password,
+  }) async {
     try {
       final dbInstance = SupabaseDb.supabase;
       final response = await dbInstance.auth.signUp(
@@ -22,5 +24,40 @@ class SupabaseAuth {
       print("Houve um error por motivos de $errorType em $erroCode");
     }
     return false;
+  }
+
+  static Future<void> signIn({
+    required String email,
+    required String passsword,
+  }) async {
+    try {
+      final dbInstance = SupabaseDb.supabase;
+      final response = await dbInstance.auth.signInWithPassword(
+        email: email,
+        password: passsword,
+      );
+
+      if (response.user?.email != null) {
+        print("Usuário logado com sucesso");
+      } else {
+        print("Não foi possivel logar o usuáro");
+      }
+    } on AuthException catch (errorType, erroCode) {
+      print("Houve um error por motivos de $errorType em $erroCode");
+    } catch (errorType, erroCode) {
+      print("Houve um error por motivos de $errorType em $erroCode");
+    }
+  }
+
+  static Future<void> signOut() async {
+    try {
+      final dbInstance = SupabaseDb.supabase;
+      final response = await dbInstance.auth.signOut();
+      print("deslogado com sucesso");
+    } on AuthException catch (errorType, erroCode) {
+      print("Houve um error por motivos de $errorType em $erroCode");
+    } catch (errorType, erroCode) {
+      print("Houve um error por motivos de $errorType em $erroCode");
+    }
   }
 }
