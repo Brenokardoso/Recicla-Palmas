d9fef6e5e25338f385ef622d7fe74083f9b6c7af
Breno Kardoso <brenopositivo@hotmail.com>
2024-12-02 17:44:41 -0300

    fix

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -3,7 +3,7 @@ import 'package:recicla_palmas/app/core/database/supabase_db.dart';
 import 'package:recicla_palmas/app/core/themes/custom_colors.dart';
 import 'package:recicla_palmas/app/core/utils/routes.dart';
 import 'package:recicla_palmas/app/core/widgets/app_bar.dart';
-import 'package:recicla_palmas/app/core/widgets/custom_page_header.dart';
+import 'package:recicla_palmas/app/core/widgets/custom_page_itens.dart';
 import 'package:recicla_palmas/app/core/widgets/drawer.dart';
 import 'package:recicla_palmas/app/core/widgets/drawer_itens.dart';
 import 'package:recicla_palmas/app/core/widgets/image.dart';
